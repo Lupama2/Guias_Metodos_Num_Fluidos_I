12f40e260126ffe719f300512ffde72f5fba7f05
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2022-09-05 12:31:52 -0300

    Pregunto dudas y corrijo errores del código
    Pregunto las dudas que me quedaron y arreglo unos errores de tipeo que me afectaban a la convergencia de ambos métodos

--- Guias/Guia_1/ejercicio_6.m
+++ Guias/Guia_1/ejercicio_6.m
@@ -13,14 +13,12 @@ XR = 1.0; #X final
 SIZE_DOMAIN = (XR-XL); #Tamanho del dominio
 UL = 0.; #Condicion Dirichlet izquierda
 UR = 1.; #Condicion Dirichlet derecha
-K = 6; #Nro de términos considerados en la suma
+#Nn numero de nodos
+#K nro de términos considerados en la suma
 
 #---------------------------------------------
 #Datos para la discretizacion
 #---------------------------------------------
-
-Nn = 23; #Numero de nodos
-
 
 
 #término extra en la EDO: y'' - y = g(x)
@@ -54,8 +52,9 @@ endfunction
 #---------------------------------------------
 #MÉTODO DE DIFERENCIAS FINITAS CENTRADAS DE 2DO ORDEN
 #---------------------------------------------
-function DC_ysol = DC_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
-    # Función que resuelve el problema con diferencias centradas
+function [DC_x, DC_ysol] = DC_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
+    # Función que resuelve el problema con diferencias centradas y devuelve los puntos en los que
+    # se evaluó la solución y los valores de la solución aproximada en dichos puntos
     # Nn: nro de nodos
     # K: nro de términos considerados en la suma de la EDO
     # UL: Condicion Dirichlet izquierda
@@ -76,37 +75,58 @@ function DC_ysol = DC_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
     DC_S3 = sparse([2:Nn], [1:Nn-1],(1/h2)*ones(1,Nn-1), Nn, Nn, 0);
     A = DC_S1 + DC_S2 + DC_S3;
 
-    # Vectores de solution
+    #Vector solución
     DC_ysol = zeros(Nn,1);
 
-    # Termino independiente  A * ysol = b
+    #Termino independiente  A * ysol = b
 
-    b = zeros(Nn,1);
+    #b = zeros(Nn,1);
     b = g(x,K); #x es un vector. Se puede meter vectores como argumentos de funciones!
     b(1) = b(1) - UL/h2;
-    b(1) = b(Nn) - UR/h2;
+    b(Nn) = b(Nn) - UR/h2;
 
     #Resolucion del problema
 
     DC_ysol=A\b.';  # SIEMPRE USAR EL COMANDO "\" PARA RESOLVER SISTEMAS LINEALES - MAS ECONOMICO
     #El operador ".'" calcula la transpuesta. Esto es necesario porque el vector b es un vector fila.
-
-
-
+    DC_x = x;
 
 endfunction
 
 %Para hallar la solución numérica:
-%DC_ysol = DC_num_sol(Nn,K,UL,UR,x,h,h2);
+%[DC_x, DC_ysol] = DC_num_sol(Nn,K,UL,UR,x,h,h2);
 %Ploteo:
-%plot(x,DC_ysol,".","markersize", 10);
+%plot(DC_x,DC_ysol,".","markersize", 10);
 %pause(10);
+
+
+#Testeo el funcionamiento de la función anterior. El siguiente código muestra que la solución converge
+#para N grande. Falta ver si converge a la solución exacta
+
+% [DC_x, DC_ysol] = DC_num_sol(23,6,UL,UR,SIZE_DOMAIN);
+% plot(DC_x,DC_ysol,".","markersize", 10);
+% hold on;
+% [DC_x, DC_ysol] = DC_num_sol(15,6,UL,UR,SIZE_DOMAIN);
+% plot(DC_x,DC_ysol,".","markersize", 10);
+% hold on;
+% [DC_x, DC_ysol] = DC_num_sol(55,6,UL,UR,SIZE_DOMAIN);
+% plot(DC_x,DC_ysol,".","markersize", 10);
+% hold on;
+% [DC_x, DC_ysol] = DC_num_sol(105,6,UL,UR,SIZE_DOMAIN);
+% plot(DC_x,DC_ysol,".","markersize", 10);
+% hold on;
+% [DC_x, DC_ysol] = DC_num_sol(205,6,UL,UR,SIZE_DOMAIN);
+% plot(DC_x,DC_ysol,".","markersize", 10);
+% hold on;
+% pause(10)
+
+
 
 
 #---------------------------------------------
 #MÉTODO DE PADÉ
 #---------------------------------------------
-function P_ysol = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
+function [P_x, P_ysol] = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
     # Función que resuelve el problema con el método de Padé
     # Nn: nro de nodos
     # K: nro de términos considerados en la suma de la EDO
@@ -135,13 +155,13 @@ function P_ysol = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
 
     #Creo la matriz B2
     #Nodos internos:
-    P_S4 = sparse([2:Nn-1], [2:Nn-1], (-2)*ones(1,Nn-2), Nn, Nn, 0);
-    P_S5 = sparse([2:Nn-1], [1:Nn-2], (1)*ones(1,Nn-2), Nn, Nn, 0);
-    P_S6 = sparse([2:Nn-1], [3:Nn],(1)*ones(1,Nn-2), Nn, Nn, 0);
+    P_S4 = sparse([2:Nn-1], [2:Nn-1], (-2/h2)*ones(1,Nn-2), Nn, Nn, 0);
+    P_S5 = sparse([2:Nn-1], [1:Nn-2], (1/h2)*ones(1,Nn-2), Nn, Nn, 0);
+    P_S6 = sparse([2:Nn-1], [3:Nn],(1/h2)*ones(1,Nn-2), Nn, Nn, 0);
 
     #Nodos de frontera:
-    P_F3 = sparse([1,1], [1,2], [-2,1], Nn, Nn, 0);
-    P_F4 = sparse([Nn,Nn], [Nn-1,Nn], [1,-2], Nn, Nn, 0);
+    P_F3 = sparse([1,1], [1,2], [-2/h2,1/h2], Nn, Nn, 0);
+    P_F4 = sparse([Nn,Nn], [Nn-1,Nn], [1/h2,-2/h2], Nn, Nn, 0);
 
     B2 = P_S4 + P_S5 + P_S6 + P_F3 + P_F4;
 
@@ -155,70 +175,92 @@ function P_ysol = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN)
 
     #Resuelvo el problema
     P_ysol = (B2-A2)\(A2*g(x,K).' - c);
+    P_x = x;
 
 endfunction
 
-%Para hallar la solución numérica:
-%P_ysol = P_num_sol(Nn,K,UL,UR,x,h,h2);
-#Ploteo:
-%plot(x,P_ysol,".","markersize", 10);
-%pause(10)
+#Testeo el funcionamiento de la función anterior. El siguiente código muestra que la solución converge
+#para N grande. Falta ver si converge a la solución exacta
+
+% [P_x, P_ysol] = P_num_sol(23,6,UL,UR,SIZE_DOMAIN);
+% plot(P_x,P_ysol,".","markersize", 10);
+% hold on;
+% [P_x, P_ysol] = P_num_sol(15,6,UL,UR,SIZE_DOMAIN);
+% plot(P_x,P_ysol,".","markersize", 10);
+% hold on;
+% [P_x, P_ysol] = P_num_sol(55,6,UL,UR,SIZE_DOMAIN);
+% plot(P_x,P_ysol,".","markersize", 10);
+% hold on;
+% [P_x, P_ysol] = P_num_sol(105,6,UL,UR,SIZE_DOMAIN);
+% plot(P_x,P_ysol,".","markersize", 10);
+% hold on;
 
 
-#---------------------------------------------
-#INCISO e
-#---------------------------------------------
-K = 6;
-Nn = 23;
 
-#Gráfico de solución exacta y ambas aproximaciones:
-
-Nnsol = 100; #Numero de nodos
-hsol = SIZE_DOMAIN / (Nnsol+1); #Distancia entre nodos
-xsol =hsol*(1:Nnsol); #Array de 1/h hasta Nn/h
-ysol = y(xsol);
-
-Nn = 81;
-
-DC_ysol = DC_num_sol(Nn,K,UL,UR,SIZE_DOMAIN);
-P_ysol = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN);
-
-plotear = true;
-
-if(plotear == true)
-
-    h = SIZE_DOMAIN / (Nn+1);
-    x=h*(1:Nn);
-
-    plot(xsol,ysol,";Exacta;","linewidth", 2);
-    hold on
-    plot(x,DC_ysol,".","markersize", 10,x,DC_ysol,";DC;","linewidth", 1);
-    hold on
-    plot(x,P_ysol,".","markersize", 10,x,P_ysol,";P;","linewidth", 1);
-    pause(10)
-endif
-
-#Grafico el error en el punto central x = 0.5 en función de h
-#El punto central es x((Nn+1)/2)
-
-
-Nn_array = [5,7,9,11,15,21,41,81,161,321]; %tienen que ser valores impares
-h_array = zeros(length(Nn_array),1).';
-DC_error = zeros(length(Nn_array),1).';
-P_error = zeros(length(Nn_array),1).';
-for ii=1:length(Nn_array)
-    DC_ysol = DC_num_sol(Nn_array(ii),K,UL,UR,SIZE_DOMAIN);
-    P_ysol = P_num_sol(Nn_array(ii),K,UL,UR,SIZE_DOMAIN);
-    DC_error(ii) = y(0.5) - DC_ysol((Nn_array(ii)+1)/2);
-    P_error(ii) = y(0.5) - P_ysol((Nn_array(ii)+1)/2);
-
-    h_array(ii) = SIZE_DOMAIN / (Nn_array(ii)+1); 
-endfor
-
-
-%plot(h_array,DC_error,".");
-%hold on
-plot(h_array,P_error,".");
+#Comparo la solución con ambos métodos para N grande:
+[P_x, P_ysol] = P_num_sol(105,6,UL,UR,SIZE_DOMAIN);
+plot(P_x,P_ysol,".","markersize", 10);
+hold on;
+[DC_x, DC_ysol] = DC_num_sol(205,6,UL,UR,SIZE_DOMAIN);
+plot(DC_x,DC_ysol,".","markersize", 10);
+hold on;
 pause(10)
+
+
+% #---------------------------------------------
+% #INCISO e
+% #---------------------------------------------
+% K = 6;
+% Nn = 23;
+
+% #Gráfico de solución exacta y ambas aproximaciones:
+
+% Nnsol = 100; #Numero de nodos
+% hsol = SIZE_DOMAIN / (Nnsol+1); #Distancia entre nodos
+% xsol =hsol*(1:Nnsol); #Array de 1/h hasta Nn/h
+% ysol = y(xsol);
+
+% Nn = 81;
+
+% DC_ysol = DC_num_sol(Nn,K,UL,UR,SIZE_DOMAIN);
+% P_ysol = P_num_sol(Nn,K,UL,UR,SIZE_DOMAIN);
+
+% plotear = true;
+
+% if(plotear == true)
+
+%     h = SIZE_DOMAIN / (Nn+1);
+%     x=h*(1:Nn);
+
+%     plot(xsol,ysol,";Exacta;","linewidth", 2);
+%     hold on
+%     plot(x,DC_ysol,".","markersize", 10,x,DC_ysol,";DC;","linewidth", 1);
+%     hold on
+%     plot(x,P_ysol,".","markersize", 10,x,P_ysol,";P;","linewidth", 1);
+%     pause(10)
+% endif
+
+% #Grafico el error en el punto central x = 0.5 en función de h
+% #El punto central es x((Nn+1)/2)
+
+
+% Nn_array = [5,7,9,11,15,21,41,81,161,321]; %tienen que ser valores impares
+% h_array = zeros(length(Nn_array),1).';
+% DC_error = zeros(length(Nn_array),1).';
+% P_error = zeros(length(Nn_array),1).';
+% for ii=1:length(Nn_array)
+%     DC_ysol = DC_num_sol(Nn_array(ii),K,UL,UR,SIZE_DOMAIN);
+%     P_ysol = P_num_sol(Nn_array(ii),K,UL,UR,SIZE_DOMAIN);
+%     DC_error(ii) = y(0.5) - DC_ysol((Nn_array(ii)+1)/2);
+%     P_error(ii) = y(0.5) - P_ysol((Nn_array(ii)+1)/2);
+
+%     h_array(ii) = SIZE_DOMAIN / (Nn_array(ii)+1); 
+% endfor
+
+
+% %plot(h_array,DC_error,".");
+% %hold on
+% plot(h_array,P_error,".");
+% pause(10)
 
 
--- Guias/Guia_1/ejercicio_6.md
+++ Guias/Guia_1/ejercicio_6.md
@@ -2,11 +2,27 @@ Readme.md
 
 DUDAS:
 
+*El método de Padé me da que no necesito en ningún caso usar b2 y me queda una diferencia centrada de orden 2 en ambos extremos. Es así? Sí.
+
+
 *La solución tiene que ser para K=6? o para K grande? Cómo obtener la solución?
+
+Puedo suponer que la solución exacta es aquella con N suficientemente grande? Tal que el "error" definido como la diferencia entre considerar N y N+1 (o 2N) es menor a un error relativo determinado?
+
+NO. SE SUPONE QUE SABEMOS RESOLVERLA EXACTAMENTE. Tenemos que encontrar una solución homogénea y una particular. 
+
+
 
 *e. Cómo determino el orden de truncamiento en ese punto?
 
+Hago un gráfico de log error en función de log h?? Sí
+Error_j = |y_j - y^~_j|
+y grafico Error_j en función de DeltaX en escala log-log. Padé es orden 4 y DFC es orden 2.
 
 
 Arreglar en el código:
-Creo que hay un error en el código de Padé porque la solución cambia bruscamente con Nn
+*X Creo que hay un error en el código de Padé porque la solución cambia bruscamente con Nn. Encontré el problema pero las soluciones convergen a funciones distintas. Para saber dónde está el error tengo que graficar la función solución.
+
+*X Ver si ambas aproximaciones convergen a la solución exacta para N grande. No lo hacen, creo que hay un error en alguna de ellas.
+
+Encontré el error en ambas. Ahora van a converger a la misma función. Falta ver que esa sea la función solución
