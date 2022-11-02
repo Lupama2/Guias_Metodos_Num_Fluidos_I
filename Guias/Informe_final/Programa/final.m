7f5b114044b65e281e2d9a94810e970aed61c156
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2022-11-02 17:23:50 -0300

    Update
    1. Me fijé si el código comienza siempre desde cero o "ve" los resultados de la ejecución anterior
    2. Vi si podía implementar CN a partir de EI. No me salió aún

--- Guias/Informe_final/Programa/final.m
+++ Guias/Informe_final/Programa/final.m
@@ -27,7 +27,49 @@ Chehade_NSdc2(Re,n1, dt,nsimpler, termino_advectivo, @U0_top)
 % fid = 3
 % ans = 0
 
+#Luego de varias veces:
+% fid = 3
+% ans = Tiempo de Calculo en segundos para
+% Ndeltat = 80
+% ans = iteraciones
+% ans = 42.019
+% ans = 0
+% fid = 3
+% ans = 0
+
+#Al inicio:
+% fid = 3
+% ans = Tiempo de Calculo en segundos para
+% Ndeltat = 80
+% ans = iteraciones
+% ans = 40.959
+% ans = 0
+% fid = 3
+% ans = 0
+
+#Al inicio nuevamente:
+% fid = 3
+% ans = Tiempo de Calculo en segundos para
+% Ndeltat = 80
+% ans = iteraciones
+% ans = 39.309
+% ans = 0
+% fid = 3
+% ans = 0
+
 
 
 #Tuve que eliminar el clear del inicio del código. No sé en qué afectará
 #Terminar de programar QUICK
+
+
+% Duda:
+% 1. Identifiqué bien cómo hacer solo un UP1, un DC2 y un QUICK?
+% 2. En QUICK solo tengo que modificar el loop que dice "#Interior desde i=2 hasta n1-1 y desde j=2 hasta n1:", no?
+% 3. En ese loop viejo se multiplica por dx*uwest(i,j) para directamente calcular un término en el cálculo de la integral 2 (ver mis notas)? O sea que para implementar QUICK eso no lo tengo que cambiar, no?
+% 4. Está bien dónde cambié la velocidad Utop1? Está cerca de la línea 620
+% 5. Cuando ejecuto el código aparece el resultado
+% % ans = 39.309
+% Pero no siempre aparece el mismo valor. El código no es determinista?
+% 6. Euler implícito solo se usa en la integral 1, no?
+% 7. No entiendo cómo aplicar CN a partir de EI. Por qué tengo que usar la mitad del paso de tiempo?
